cbd63df578bfdbec2be5f33f8c8706bf1e2170be
Lisa Williams <lisaismyname0@gmail.com>
2022-06-30 17:00:40 -0400

    playing around with adding Artist class, Favorite class, and user app

--- Album/models.py
+++ Album/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from users.models import User
 
 
 class Album(models.Model):
@@ -11,3 +12,21 @@ class Album(models.Model):
 
     def __str__(self):
         return f"{self.album_name}"
+
+
+class Artist(models.Model):
+    artist_name = "artist_name"
+    artists_albums = ["artist_album1", "artist_album2"]
+
+    album = models.ForeignKey(
+        "Album", on_delete=models.CASCADE, related_name="artist", blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.artist_name}"
+
+
+class Favorite(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="favorites")
+    album = models.ForeignKey(
+        Album, on_delete=models.CASCADE, related_name="favorites")
