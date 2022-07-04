467bab88e187a97a926f37b14b3af376f4be4b39
Lisa Williams <lisaismyname0@gmail.com>
2022-07-03 21:58:12 -0400

    troubleshooting

--- django_music/settings.py
+++ django_music/settings.py
@@ -34,9 +34,9 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'django.contrib.sessions',
+    'django.contrib.segit ssions',
     'django.contrib.messages',
-    'django.contrib.staticfiles', "Album", "users",
+    'django.contrib.staticfiles', "Album",
 ]
 
 MIDDLEWARE = [
